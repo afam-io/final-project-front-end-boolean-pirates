bcbc8345387a4403a88b5bdcee50bf7de3537886
AfamOkoh <afamokoh@gmail.com>
2022-07-28 12:07:54 +0100

    build: rebuilt project using next.js boiler plate without auth0

---
+++ components/Header.jsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <div className='bg-red-600 w-full h-48'>header</div>;
+};
+
+export default Header;
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Header from '../components/header';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <div className='bg-black w-full h-5 text-white'>current version</div>
+    </>
+  );
+}
