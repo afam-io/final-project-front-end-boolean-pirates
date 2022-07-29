1e5649c1c91ab6246ea10cdff96dfa661fbaf871
You <joeyk2k@hotmail.com>
2022-07-29 14:53:36 +0100

    style: homepage layout

--- components/Card.jsx
+++ components/Card.jsx
@@ -2,9 +2,7 @@ const Card = ({
   imageURL,
   title,
   description,
-  hashtag1,
-  hashtag2,
-  hashtag3,
+  materials
 }) => {
   return (
     <div>
@@ -14,10 +12,12 @@ const Card = ({
           <div className="font-bold text-xl mb-2">{title}</div>
           <p className="text-gray-700 text-base">{description}</p>
         </div>
-        <div className="px-6 pt-4 pb-2">
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            {hashtag1}
+        <div className="px-6 pt-4 pb-2" >
+        {materials.map((material, index) => 
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2" key={index}>
+            {material}
           </span>
+        )}
         </div>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -3,30 +3,63 @@ import Card from "../components/card.jsx";
 import Image from "next/image";
 
 export default function Home() {
+
+  const mockData = [{
+    imageURL: "https://opensourcelowtech.org/tutorials/dish/images/100_7348.JPG",
+    title: "Wifi/Mobile Reflector Dish",
+    description: "This is a paraboloidal reflector dish, more or less a sattelite dish, that can easily be made from one sheet of plywood or other board, chicken wire or other mesh, and cable/zip ties. It works by reflecting wifi or mobile signal and focusing them onto a receiver such as your phone, USB wifi adaptor, hotspotN, etc.",
+    materials:["plywood","wire","cable ties"]
+    },
+    {
+      imageURL: "https://opensourcelowtech.org/tutorials/dish/images/100_7348.JPG",
+      title: "Wifi/Mobile Reflector Dish",
+      description: "This is a paraboloidal reflector dish, more or less a sattelite dish, that can easily be made from one sheet of plywood or other board, chicken wire or other mesh, and cable/zip ties. It works by reflecting wifi or mobile signal and focusing them onto a receiver such as your phone, USB wifi adaptor, hotspotN, etc.",
+      materials:["plywood","wire","cable ties"]
+      },
+    {
+      imageURL: "https://opensourcelowtech.org/tutorials/dish/images/100_7348.JPG",
+      title: "Wifi/Mobile Reflector Dish",
+      description: "This is a paraboloidal reflector dish, more or less a sattelite dish, that can easily be made from one sheet of plywood or other board, chicken wire or other mesh, and cable/zip ties. It works by reflecting wifi or mobile signal and focusing them onto a receiver such as your phone, USB wifi adaptor, hotspotN, etc.",
+      materials:["plywood","wire","cable ties"]
+      },
+      {
+        imageURL: "https://opensourcelowtech.org/tutorials/dish/images/100_7348.JPG",
+        title: "Wifi/Mobile Reflector Dish",
+        description: "This is a paraboloidal reflector dish, more or less a sattelite dish, that can easily be made from one sheet of plywood or other board, chicken wire or other mesh, and cable/zip ties. It works by reflecting wifi or mobile signal and focusing them onto a receiver such as your phone, USB wifi adaptor, hotspotN, etc.",
+        materials:["plywood","wire","cable ties"]
+        }]
+
   return (
     <>
       <Header />
-      <div className="bg-black w-full h-5 text-white">current version</div>
+      <div className="relative justify-self-center">
       <Image
-        src="https://static.vecteezy.com/system/resources/previews/004/828/251/large_2x/working-together-to-protect-the-environment-earth-day-two-hands-hold-the-two-green-worlds-together-environment-and-reduce-global-warming-help-the-world-in-ecological-sustainability-concept-free-photo.jpg"
-        width={2835}
-        height={1960}
+        src="https://static.vecteezy.com/system/resources/previews/004/828/251/large_2x/working-together-to-protect-the-environment-earth-day-two-hands-hold-the-two-green-worlds-together-environment-and-reduce-global-warming-help-the-world-in-ecological-sustainability-concept-free-photo.jpg"  
+        width={1425}
+        height={980}
+        alt="main-image"
       />
-      <div>
-        <h2>
+      <div className="absolute bottom-8 left-0 right-0 px-4 py-2 bg-gray-300 opacity-70">
+        <h1 className="mt-2 text-4xl text-black-300">
           From wind turbines to window farms, find your next project with
           Redeem.
-        </h2>
+        </h1>
       </div>
-
+      </div>
+      <div className="flex items-center justify-center">
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      {mockData.map((mockData, index) => 
+      <div key={index} className="m-2">
       <Card
-        imageURL="/img/card-top.jpg"
-        title="Frozen Lake"
-        description=" Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-        hashtag1="cheese"
-        hashtag2="bread"
-        hashtag3="cake"
+        imageURL={mockData.imageURL}
+        title={mockData.title}
+        description={mockData.description}
+        materials={mockData.materials}
       />
+      </div>
+      )}
+      </div>
+      </div>
     </>
   );
 }
