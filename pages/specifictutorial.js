e47d4571ace1a3260296817324564c9d78d32c8e
Mustafa <Mustafa.mohamad239@gmail.com>
2022-08-01 13:48:12 +0100

    feat: Added specifictutorials pages, made the specifictutorialscardcomponent and made libs folder for mock data

---
+++ pages/specifictutorial.js
@@ -0,0 +1,9 @@
+import SpecificTutorialCard from "../components/SpecificTutorialCard"
+
+function specifictutorial() {
+  return (
+    <SpecificTutorialCard/>
+  )
+}
+
+export default specifictutorial
