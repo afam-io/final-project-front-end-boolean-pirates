02d068925ca5d141ea8a5485d9976c978cc846c6
You <joeyk2k@hotmail.com>
2022-08-01 16:03:01 +0100

    fix: navbar responsive, tweaked image

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,124 +9,142 @@ import {
 	AiOutlineUser,
 	AiOutlineHeart,
 	AiOutlineContacts,
-  AiOutlineMenu,
+	AiOutlineMenu,
 } from "react-icons/ai";
+import Link from "next/link";
+import createtutorials from './../pages/createtutorials';
 
 const Header = () => {
 	const [isOpen, setIsOpen] = useState(false);
 	return (
 		<div>
+		<div>
 			{/* wrapper for the whole nav bar */}
-			<nav className="bg-green-backgroundtext ">
+			<nav className="bg-green-backgroundtext">
 				{/* logo */}
 				{/* TODO need to work on the sizing for the image */}
 				<Image
 					className="h-8 float-left mt-2 ml-3"
 					src="/images/logo.png"
 					alt="logo"
-					width="300px"
-					height="61px"
+					width="250px"
+					height="50px"
 				/>
 				{/* login button */}
-				<p className=" h-6 float-right bg-white-cardbackground hover:bg-gray-700 black-text font-bold px-2 m-3 rounded-3xl">
+				<p className=" h-6 float-right bg-white-cardbackground hover:bg-gray-500 black-text font-bold px-2 m-4 rounded-3xl hover:cursor-pointer">
 					Login
 				</p>
 				{/* wrapper for the whole navbar */}
-				<div className="max-w-fit mx-auto px-4 sm:px-6 lg:px-8">
+				<div className="max-w-fit mx-auto">
 					{/* links in the middle of the navbar */}
 					<div className="flex items-center justify-between h-30">
 						<div className="flex items-center">
 							{/* styles for the mobile menu */}
-						
 							<div className=" flex-shrink-0 ml-0 flex items-baseline space-x-6 text-white px-0 py-2 rounded-md text-sm font-medium">
 								{/* individual mobile links wrapper */}
 								<div className="md:hidden ml-0 flex items-baseline space-x-6">
 									{/* wrapper for individual mobile link */}
-									<div className="block items-center justify-center px-7 rounded-md text-white hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+									<Link href="/">
+									<div className="block items-center justify-center sm:px-7 px-2 rounded-md text-white hover:text-white hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-500 focus:ring-white hover:cursor-pointer">
 										{/* wrapper for navbar icon and the icon */}
 										<div className="block h-6 w-6 text-4xl mb-2">
 											<AiOutlineHome />
 										</div>
 										<p>Home</p>
 									</div>
+									</Link>
 									{/* wrapper for individual mobile link */}
-									<div className="block items-center justify-center px-7 rounded-md text-white hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-									<div className="block h-6 w-6 pl-2 text-4xl mb-2">
+									<Link href="/tutorials">
+									<div className="block items-center justify-center sm:px-7 px-2 rounded-md text-white hover:text-white hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-500 focus:ring-white hover:cursor-pointer">
+										<div className="block h-6 w-6 pl-2 text-4xl mb-2">
 											<AiOutlineRead />
 										</div>
 										<p>Tutorials</p>
 									</div>
+									</Link>
 									{/* wrapper for individual mobile link */}
-									<div className="block items-center justify-center px-7 rounded-md text-white hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-									<div className="block h-6 w-6 text-4xl mb-2">
+									<Link href="/createtutorials">
+									<div className="block items-center justify-center sm:px-7 px-2 rounded-md text-white hover:text-white hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-500 focus:ring-white hover:cursor-pointer">
+										<div className="block h-6 w-6 text-4xl mb-2">
 											<AiOutlinePlus />
 										</div>
 										<p>Create</p>
 									</div>
-               
+									</Link>
 								</div>
 							</div>
 
 							<div className="hidden md:block ">
 								{/* wrapper for desktop view middle link items */}
 								{/* TODO need to space out these links for the mobile site */}
-								<div className="flex justify-evenly max-w-full w-screen">
-                  <a
+								<div className="flex mr-10 justify-around max-w-full w-screen ">
+									<Link href="/">
+									<a
 										href="#"
-										className= "hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+										className="hover:bg-gray-500 text-white px-3 py-2 rounded-md text-sm font-medium"
 									>
 										Home
 									</a>
+									</Link>
+									<Link href="/tutorials">
 									<a
 										href="#"
-										className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+										className=" hover:bg-gray-500 text-white px-3 py-2 rounded-md text-sm font-medium"
 									>
 										Tutorials
 									</a>
+									</Link>
+									<Link href="/createtutorials">
 									<a
 										href="#"
-										className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+										className=" hover:bg-gray-500 text-white px-3 py-2 rounded-md text-sm font-medium"
 									>
 										Create
 									</a>
+									</Link>
+									<Link href="/tutorials">
 									<a
 										href="#"
-										className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+										className=" hover:bg-gray-500 text-white px-3 py-2 rounded-md text-sm font-medium"
 									>
 										Search
 									</a>
-
+									</Link>
+									<Link href="/">
 									<a
 										href="#"
-										className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+										className=" hover:bg-gray-500 text-white px-3 py-2 rounded-md text-sm font-medium"
 									>
 										Profile
 									</a>
-
+									</Link>
+									<Link href="/">
 									<a
 										href="#"
-                    className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+										className=" hover:bg-gray-500 text-white px-3 py-2 rounded-md text-sm font-medium"
 									>
 										Favourites
 									</a>
-
+									</Link>
+									<Link href="/">
 									<a
 										href="#"
-                    className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+										className=" hover:bg-gray-500 text-white px-3 py-2 rounded-md text-sm font-medium"
 									>
 										Events
 									</a>
+									</Link>
 								</div>
 							</div>
 						</div>
 						{/* wrapper around the more button */}
-						<div className="flex md:hidden justify-center ml-5">
+						<div className="flex md:hidden justify-center">
 							{/* mobile more button */}
 							{/* TODO need to align this button with icons */}
 							<button
 								onClick={() => setIsOpen(!isOpen)}
 								type="button"
-								className="inline-flex items-center justify-center pt-5 ml-5 h-12 w-16 rounded-md text-white hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+								className="inline-flex items-center justify-center pr-5 pt-5 ml-10 pl-8 mr-5 h-12 w-25 rounded-md text-white hover:text-white hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-500 focus:ring-white"
 								aria-controls="mobile-menu"
 								aria-expanded="false"
 							>
@@ -165,17 +183,14 @@ const Header = () => {
 											/>
 										</svg>
 									)}
-									<p className="text-white pb-5 pr-3 rounded-md text-sm font-medium">More</p>
+									<p className="text-white pb-5 pr-3 rounded-md text-sm font-medium">
+										More
+									</p>
 								</div>
 							</button>
-					
 						</div>
-						
 					</div>
-					
 				</div>
-				
- 
 				<Transition
 					show={isOpen}
 					enter="transition ease-out duration-100 transform"
@@ -188,61 +203,60 @@ const Header = () => {
 					{(ref) => (
 						<div className="md:hidden" id="mobile-menu ">
 							<div ref={ref} className="px-2 pt-2 space-y-1 sm:px-3">
+							<Link href="tutorials">
 								<a
 									href="#"
-									className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
+									className="hover:bg-gray-500 text-white block px-3 py-2 rounded-md text-base font-medium"
 								>
-                {/* TODO size the icons */}
-									{" "}
+									{/* TODO size the icons */}{" "}
 									<div className="block h-6 w-6 text-3xl mb-2">
-                  <AiOutlineSearch />
-                  </div>
-                 
+										<AiOutlineSearch />
+									</div>
 									Search
 								</a>
-
+								</Link>
+								<Link href="/">
 								<a
 									href="#"
-									className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+									className="text-white hover:bg-gray-500 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
 								>
 									{" "}
-                  <div className="block h-6 w-6 text-3xl mb-2">
-                  <AiOutlineUser />
-                  </div>
-							
+									<div className="block h-6 w-6 text-3xl mb-2">
+										<AiOutlineUser />
+									</div>
 									Profile
 								</a>
-
+								</Link>
+								<Link href="/">
 								<a
 									href="#"
-									className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+									className="text-white hover:bg-gray-500 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
 								>
 									{" "}
-                  <div className="block h-6 w-6 text-3xl mb-2">
-                  <AiOutlineHeart />
-                  </div>
-									
+									<div className="block h-6 w-6 text-3xl mb-2">
+										<AiOutlineHeart />
+									</div>
 									Favourites
 								</a>
-
+								</Link>
+								<Link href="/">
 								<a
 									href="#"
-									className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+									className="text-white hover:bg-gray-500 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
 								>
 									{" "}
-                  <div className="block h-6 w-6 text-3xl mb-2">
-                	<AiOutlineContacts />
-                  </div>
-								
+									<div className="block h-6 w-6 text-3xl mb-2">
+										<AiOutlineContacts />
+									</div>
 									Events
 								</a>
+								</Link>
 							</div>
 						</div>
 					)}
 				</Transition>
-			
-				
 			</nav>
+		</div>
 		</div>
 	);
 };
