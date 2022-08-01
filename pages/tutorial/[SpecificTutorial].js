037b0d99c5cf6ee35c0f4f0b003e4d6aab863359
AfamOkoh <afamokoh@gmail.com>
2022-08-01 14:35:39 +0100

    feat: moved the import of specific tutorial component to the dynamically rendered page

---
+++ pages/tutorial/[SpecificTutorial].js
@@ -0,0 +1,18 @@
+import { useRouter } from 'next/router';
+import SpecificTutorialCard from '../../components/SpecificTutorialCard';
+import specificTutorialData from '../../libs/specificTutorialData';
+
+function SpecificTutorial() {
+  const router = useRouter();
+  const { SpecificTutorial } = router.query;
+
+  return (
+    <>
+      <h1>Title: {SpecificTutorial} </h1>{' '}
+      {/* To show clearly that the dynamic routing is working*/}
+      <SpecificTutorialCard tutorialData={specificTutorialData} />
+    </>
+  );
+}
+
+export default SpecificTutorial;
