f748be71d2f6a8e7465e54e83de81a3cf7d17f87
HairiBeast <shpetzla@gmail.com>
2022-08-02 12:07:03 +0100

    Search bar has a wider default size and dynamically resizes. Search button is smaller and better fits smaller screens

--- pages/tutorials.js
+++ pages/tutorials.js
@@ -70,15 +70,29 @@ export default function tutorials() {
             <input
               type="search"
               id="default-search"
-              className="grid p-4 pl-10 w-full text-sm shadow-md focus:outline-none focus:ring-green-500 focus:border-green-500 mt-1 sm:text-sm border border-gray-300 rounded-lg"
-              placeholder="Search Mockups, Logos..."
+              className="grid p-4 pl-10 w-full text-sm shadow-md focus:outline-none focus:ring-green-500 focus:border-green-500 mt-1 sm:text-sm border border-gray-300 rounded-xl"
+              placeholder="Search Tutorials, Materials..."
               required
             />
             <button
               type="submit"
-              className="text-white absolute right-2.5 bottom-2.5 bg-green-700 hover:bg-green-backgroundtext focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-green-backgroundtext dark:hover:bg-green-700 dark:focus:ring-green-800"
+              className="absolute right-1.5 bottom-1.5 p-2.5 ml-2 text-sm font-medium text-white bg-green-backgroundtext rounded-xl border border-green-700 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-700 dark:hover:bg-green-backgroundtext dark:focus:ring-green-800"
             >
-              Search
+              <svg
+                className="w-5 h-5"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                ></path>
+              </svg>
+              <span className="sr-only">Search</span>
             </button>
           </div>
         </form>
