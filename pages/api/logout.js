5d411adc7145010bf73910b94ea4bacf14dca045
You <joeyk2k@hotmail.com>
2022-08-02 15:41:13 +0100

    feature: init Auth0 and changed login button

---
+++ pages/api/logout.js
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import auth0 from '../../lib/auth0'
+
+const logout = async (req, res) => {
+  try {
+    await auth0.handleLogout(req, res)
+  } catch (error) {
+    console.error(error)
+    const errorMessage =
+      error instanceof Error ? error.message : 'Internal server error'
+    res.status(500).end(errorMessage)
+  }
+}
+
+export default logout
