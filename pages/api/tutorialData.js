c6c6cccd4194800c0ed18fe3eb86c7e5e783cca0
AfamOkoh <afamokoh@gmail.com>
2022-08-02 13:59:53 +0100

    feat: work on adding getbyId to the tutorial api

--- pages/api/tutorialData.js
+++ pages/api/tutorialData.js
@@ -1,6 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import tutorialData from '../../libs/specificTutorialData';
+import tutorialData from '../../library/specificTutorialData';
 
 export default function handler(req, res) {
+  if (req.query.params) {
+    tutorialId = req.query.params;
+  }
   res.status(200).json(tutorialData);
+}
+
+export function getTutorialById(id) {
+  const found = tutorialData.find(function (book) {
+    return book.id === id;
+  });
+  return found;
 }
