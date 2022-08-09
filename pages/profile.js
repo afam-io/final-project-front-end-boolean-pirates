b53ad9b26a5c089b07abfe2c991a75467ac621cc
Joe Klakus <joeyk2k@hotmail.com>
2022-08-09 14:09:31 +0100

    navbar not authorised show login & hide events - joe

---
+++ pages/profile.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Image from 'next/image'
+
+const profile = ({user}) => {
+  console.log(user)
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
+      <div className="font-bold text-xl p-2 mb-2">{user?.name}</div>
+      {/* <Image
+        className="w-full"
+        src={user?.picture}
+        alt={user?.sub}
+        height="450px"
+        width="600px"
+      /> */}
+     <p className="text-gray-700 p-2 text-base">{user?.nickname}</p>
+     <p className="text-gray-700 p-2 text-base">{user?.updated_at}</p>
+    </div>
+  )
+}
+
+export default profile
