4040c6dafbfa3959af3579424d96f8e31df6e231
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2022-08-10 15:12:24 +0100

    component: footer added and contact page, icons and copyright

---
+++ pages/contact.js
@@ -0,0 +1,10 @@
+const Contact = () => {
+    return (
+        <div>
+        <h1>Contact : Booleanpirates@gmail.com </h1>
+        <p> Github details here perhaps</p>
+        </div>
+      );
+}
+ 
+export default Contact;
