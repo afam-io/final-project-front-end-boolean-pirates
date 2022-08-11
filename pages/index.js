5d29af69d53a94fae5592ea59c6dd469a3bbe055
HairiBeast <shpetzla@gmail.com>
2022-08-11 15:22:14 +0100

    added homepage tagline and hero image options

--- pages/index.js
+++ pages/index.js
@@ -1,66 +1,65 @@
-import Card from '../components/Card';
-import Image from 'next/image';
+import Card from "../components/Card";
+import Image from "next/image";
 
-export default function Home( { data, user } ) {
-
-  
+export default function Home({ data, user }) {
   return (
     //whole layout
     <div>
       {/* wrapper for the image and text */}
-      <div className='relative justify-self-center w-full h-80 md:min-w-fit'>
+      <div className="relative justify-self-center w-full h-80 md:min-w-fit">
         {/* TODO fix the white space on full screen on the left */}
         <Image
-          src='/images/full-screen-hero-image.jpg'
+          src="/images/up-cycling1"
           layout="fill"
           objectFit="cover"
-          alt='main-image'
+          alt="main-image"
         />
         {/* card */}
-        <div className='lg:block absolute text-center bottom-4 left-0 right-0 px-4 py-2 p-4 bg-gray-300 opacity-70 w-10/12 mx-auto rounded-lg'>
-          <h1 className='text-xl lg:text-3xl text-green-backgroundtext'>
-            From wind turbines to window farms, find your next project with
-            Redeem.
+        <div className="lg:block absolute text-center bottom-4 left-0 right-0 px-4 py-2 p-4 bg-gray-300 opacity-70 w-10/12 mx-auto rounded-lg">
+          <h1 className="text-xl lg:text-3xl text-green-backgroundtext">
+            Connect with eco-minded creators who are looking to halt waste and
+            help the environment.
           </h1>
         </div>
       </div>
       {/* this is currently unused */}
-      <div className='hidden mt-3 text-center bottom-4 left-0 right-0 px-4 py-2 p-4 bg-gray-300 opacity-70 w-10/12 mx-auto rounded-lg'>
-          <h1 className='text-xl lg:text-3xl text-green-backgroundtext'>
-            From wind turbines to window farms, find your next project with
-            Redeem.
-          </h1>
-        </div>
+      <div className="hidden mt-3 text-center bottom-4 left-0 right-0 px-4 py-2 p-4 bg-gray-300 opacity-70 w-10/12 mx-auto rounded-lg">
+        <h1 className="text-xl lg:text-3xl text-green-backgroundtext">
+          From wind turbines to window farms, find your next project with
+          Redeem.
+        </h1>
+      </div>
       {/* card holder that aligns the cards to center */}
-      <div className='flex items-center justify-center mt-2'>
+      <div className="flex items-center justify-center mt-2">
         {/* media query which shows different amount of cards on different screen sizes */}
-        <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-1'>
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-1">
           {/* Filters the first 6 results before mapping */}
-          {data.filter((item, index) => index < 6).map((data, index) => (
-            <div key={index} className='m-2'>
-              <Card
-                user={user}
-                imageUrl={data.imageUrl}
-                title={data.title}
-                materials={data.materials}
-                likes={data.likes}
-                date={data.createdAt}
-                id={data._id}
-                creator={data.creator}
-             />
-            </div>
-          ))}
+          {data
+            .filter((item, index) => index < 6)
+            .map((data, index) => (
+              <div key={index} className="m-2">
+                <Card
+                  user={user}
+                  imageUrl={data.imageUrl}
+                  title={data.title}
+                  materials={data.materials}
+                  likes={data.likes}
+                  date={data.createdAt}
+                  id={data._id}
+                  creator={data.creator}
+                />
+              </div>
+            ))}
         </div>
       </div>
     </div>
   );
 }
 
-
 export const getServerSideProps = async () => {
-  const data = await fetch(
-    `https://backend-soc.herokuapp.com/tutorials`
-  ).then((r) => r.json());
+  const data = await fetch(`https://backend-soc.herokuapp.com/tutorials`).then(
+    (r) => r.json()
+  );
 
   return {
     props: {
@@ -68,4 +67,3 @@ export const getServerSideProps = async () => {
     },
   };
 };
-
