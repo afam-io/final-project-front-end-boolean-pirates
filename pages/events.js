fa38b565b29fff6126c96b9abfdfa3e4d340fdcd
Joe Klakus <joeyk2k@hotmail.com>
2022-08-12 14:17:04 +0100

    added text to events

--- pages/events.js
+++ pages/events.js
@@ -6,7 +6,13 @@ const DynamicComponentWithNoSSR = dynamic(() => import( '../components/Map'), {
 
 export default function MapPage() {
   return (
-    <div className="sticky mt-5 w-full h-screen">
+    <div className="sticky mt-10 w-full h-screen">
+            <div className="flex justify-center text-lg font-semibold font-sans text-green-backgroundtext sm:text-2xl text-black-300">
+        Events
+      </div>
+      <div className="flex justify-center pt-2 text-base font-normal font-sans text-green-backgroundtext">
+        Find your nearest eco-event below.
+      </div>
   <DynamicComponentWithNoSSR />
   </div>
   )
