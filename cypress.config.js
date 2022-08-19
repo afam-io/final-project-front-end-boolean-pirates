9e7e8f62d39e82a105b39b614b9d561c0f9732cb
AfamOkoh <afamokoh@gmail.com>
2022-08-19 14:55:19 +0100

    style: add hover opacity on contact page

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '648bmz',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
